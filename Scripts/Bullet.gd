8f5024014f56e56a491241c66c1f5cde3b547935
gabtes <gabentes@gmail.com>
2023-05-06 17:21:43 -0500

    Fixed import bullet bug

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,13 +1,9 @@
 extends RigidBody2D
-class_name CBullet
-
-var velocity: Vector2
-
-func _physics_process(delta):
-	apply_central_force(velocity * 1000)
-	
-func setup(_position: Vector2, _velocity: Vector2, _scene: Node):
-	_scene.add_child(self)
-	global_position = _position
+class_name CBullet  
+var velocity: Vector2  
+func _physics_process(_delta):     
+	apply_central_force(velocity * 1000)     
+func setup(_position: Vector2, _velocity: Vector2, _scene: Node):     
+	_scene.add_child(self)     
+	global_position = _position     
 	velocity = _velocity
-	
