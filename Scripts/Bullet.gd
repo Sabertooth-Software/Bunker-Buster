9498e822333b49492dcf6f6341cf5f50e3b54757
JojoMakesGames <jojo.oatey@gmail.com>
2023-05-05 19:27:40 -0500

    Spawn bullets and all that

---
+++ Scripts/Bullet.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+class_name Bullet
+
+var velocity: Vector2
+
+func _physics_process(delta):
+	apply_central_force(velocity * 1000)
+	
+func setup(_position: Vector2, _velocity: Vector2, _scene: Node):
+	_scene.add_child(self)
+	global_position = _position
+	velocity = _velocity
+	print_debug("position: ", position, "global_position: ", position)
+	
--- Scripts/MainCharacter.gd
+++ Scripts/MainCharacter.gd
@@ -4,14 +4,17 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+signal shoot(direction: Vector2)
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _process(_delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot.emit(Vector2(1,0))
+		
 
-func _physics_process(delta):
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func _physics_process(_delta):
 	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	if direction:
 		velocity = direction * SPEED
