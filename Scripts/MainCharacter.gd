5d5f42c1e0fccedcb62aae88269110a1dc46c6b6
John Oatey <112036395+JojoMakesGames@users.noreply.github.com>
2023-05-07 13:33:45 -0500

    Configure tanks a little better (#5)

--- Scripts/MainCharacter.gd
+++ Scripts/MainCharacter.gd
@@ -1,17 +1,16 @@
-extends Node2D
+extends Tank
+class_name PlayerTank
 
-@onready var tank: Tank = $Tank
 @export var bullet: PackedScene
 
 func _process(_delta):
 	if Input.is_action_just_pressed("shoot"):
 		var shooting_vector: Vector2 = get_global_mouse_position() - global_position
-		tank.shoot(shooting_vector, bullet)
+		super.shoot(shooting_vector, bullet)
 
 func _physics_process(_delta):
-	position = tank.position
-	tank.look(get_global_mouse_position())
+	super.look(get_global_mouse_position())
 	
 	var moving_vector: Vector2 = Input.get_vector("left", "right", "up", "down")
 	if moving_vector:
-		tank.move(moving_vector)
+		super.move(moving_vector)
