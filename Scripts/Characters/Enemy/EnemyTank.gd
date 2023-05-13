ef75ecde1d024331685deb4d88db56a2e3dc8cfd
gabtes <gabentes@gmail.com>
2023-05-13 00:31:32 -0500

    Added enemy movement
    Goes between position 0 and 1. Move them to alter path

--- Scripts/Characters/Enemy/EnemyTank.gd
+++ Scripts/Characters/Enemy/EnemyTank.gd
@@ -6,12 +6,43 @@ extends Tank
 
 @onready var timer: Timer = $Timer
 
+@onready var Pos0 : Node2D = $Position0
+@onready var Pos1 : Node2D = $Position1
+
+var wander_points = []
+var point_index: int = 0
+var next_point: Vector2
+var move_vector: Vector2
+
+func _get_next_point():
+	if point_index+1==wander_points.size():
+		point_index = 0
+	else :
+		point_index = point_index+1
+	next_point = wander_points[point_index]
+
+func _handle_movement():
+	if next_point==null:
+		_get_next_point()
+	var dist = global_position.distance_to(next_point)/100
+	#print(dist)
+	if dist>.2:
+		move_vector = (next_point-global_position).normalized()
+		super.move(move_vector*5)
+	else:
+		_get_next_point()
+
 func _ready():
 	if target == null:
 		var player_tank: Node = get_tree().get_current_scene().get_node("PlayerTank")
 		if player_tank is PlayerTank:
 			target = player_tank
 			_setup_timer()
+	wander_points.append(Pos0.global_position)
+	wander_points.append(Pos1.global_position)
+	print(wander_points[0])
+	print(wander_points[1])
+	next_point = wander_points[0]
 
 func on_timeout():
 	assert(target != null, "Tried to shoot without target assigned to enemy tank")
@@ -21,6 +52,7 @@ func on_timeout():
 
 func _process(_delta):
 	super.look(target.global_position)
+	_handle_movement()
 
 func _setup_timer():
 	timer.timeout.connect(on_timeout)
