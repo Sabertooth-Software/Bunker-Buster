307932b68a54e660e87a984217d361f42e3b6f2c
JojoMakesGames <jojo.oatey@gmail.com>
2023-05-13 10:54:03 -0400

    Slow down

--- Scripts/Objects/golf_ball.gd
+++ Scripts/Objects/golf_ball.gd
@@ -3,30 +3,40 @@ extends RigidBody2D
 @onready var terrain_detector: TerrainDetector = $TerrainDetector
 @onready var arrow: Node2D = $Arrow
 
+@export var terrain_map: Dictionary
+
 @export var max_shot_power: float = 100
+@export var speed_modifier: float = 500
 
 var _start_position: Vector2
 var _shoot_vector: Vector2
+var _deccel: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	terrain_detector.terrain_changed.connect(_on_terrain_chnaged)
+	_deccel = terrain_map[Terrain.Type.GRASS]
 
 func _process(_delta):
 	if Input.is_action_just_pressed("putt"):
+		arrow.visible = true
 		_start_position = get_global_mouse_position()
 	if Input.is_action_pressed("putt"):
 		_shoot_vector = _start_position - get_global_mouse_position()
-		var proportion: float = _shoot_vector.length()/max_shot_power
-		arrow.scale.y = proportion
+		var proportion: float = clampf(_shoot_vector.length()/max_shot_power, 0, 1)
+		arrow.scale.y = proportion * .75
 		arrow.rotation = _shoot_vector.normalized().angle() + (PI/2)
-		print_debug(_shoot_vector.length()/max_shot_power)
 	if Input.is_action_just_released("putt"):
-		_shoot_vector = _shoot_vector.normalized() * 100 
+		arrow.visible = false
+		_shoot_vector = _shoot_vector.normalized() * speed_modifier 
 		apply_central_impulse(_shoot_vector)
 		arrow.scale.y = 0
 		ShotCounter.increase_shots()
+		
+func _physics_process(delta):
+	var slow_vector: Vector2 = lerp(linear_velocity, Vector2.ZERO, _deccel) * -1
+	apply_central_impulse(slow_vector)
 
 
 func _on_terrain_chnaged(new_terrain: Terrain.Type):
-	print(new_terrain)
+	_deccel = terrain_map[new_terrain]
