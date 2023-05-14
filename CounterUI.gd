52e7d49bb85c9ce7c8f5dcd4fac17f3e4975b704
JojoMakesGames <jojo.oatey@gmail.com>
2023-05-14 12:10:39 -0400

    Add colors

--- CounterUI.gd
+++ CounterUI.gd
@@ -6,7 +6,15 @@ var par: int
 func _ready():
 	ShotCounter.score_update.connect(_on_score_update)
 	par = SceneManager.get_level_data().par
-	_count_text.text = "0/" + str(par)
+	_set_text(0)
 
 func _on_score_update(score: int):
+	_set_text(score)
+
+func _set_text(score: int):
 	_count_text.text = str(score) + "/" + str(par)
+	if score <= par:
+		_count_text.add_theme_color_override("default_color", Color.GREEN)
+	else:
+		_count_text.add_theme_color_override("default_color", Color.RED)
+		
