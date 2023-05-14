c51aa10cdbfc0115b4ae0ffb6aff4bf8a89793bd
JojoMakesGames <jojo.oatey@gmail.com>
2023-05-14 15:42:48 -0400

    QOL changes

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -4,7 +4,7 @@ class_name Bullet
 var velocity: Vector2  
 
 func _physics_process(_delta):     
-	apply_central_force(velocity * 250)
+	apply_central_force(velocity * 175)
    
 func setup(_position: Vector2, _velocity: Vector2, _scene: Node, _initiator: Node2D):     
 	_scene.add_child(self)
