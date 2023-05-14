e95a0187d2d53a9081adbd74ee85a43c4a7d84ec
JojoMakesGames <jojo.oatey@gmail.com>
2023-05-14 16:22:10 -0400

    Water

--- Scripts/Components/TerrainDetector.gd
+++ Scripts/Components/TerrainDetector.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name TerrainDetector
 
 var current_terrain: Terrain.Type = Terrain.Type.GRASS
+var is_on_water: bool = false
 signal terrain_changed(new_terrain: Terrain.Type)
 
 # Called when the node enters the scene tree for the first time.
@@ -15,7 +16,10 @@ func _process(_delta):
 func process_tilemap(body: TileMap, body_rid: RID):
 	var tile_coords: Vector2 = body.get_coords_for_body_rid(body_rid)
 	var tile_data: TileData
-	for i in body.get_layers_count():
+	var layer_count: int = body.get_layers_count()
+	if layer_count >= 5:
+		is_on_water = body.get_cell_tile_data(5, tile_coords) != null
+	for i in layer_count:
 		tile_data = body.get_cell_tile_data(i, tile_coords)
 		if tile_data:
 			break;
--- Scripts/Objects/golf_ball.gd
+++ Scripts/Objects/golf_ball.gd
@@ -47,6 +47,9 @@ func _process(_delta):
 func _physics_process(_delta):
 	var slow_vector: Vector2 = lerp(linear_velocity, Vector2.ZERO, _deccel) * -1
 	apply_central_impulse(slow_vector)
+	if terrain_detector.is_on_water:
+		linear_velocity = Vector2.ZERO
+		global_position = _start_position
 
 
 func _on_terrain_chnaged(new_terrain: Terrain.Type):
